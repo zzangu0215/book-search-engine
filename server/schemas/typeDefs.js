e5742834785da29d457162681eb421c956e1cf96
Junny <tajo0215@gmail.com>
2021-08-21 13:28:13 -0700

    type Book implemented

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,4 +9,13 @@ const typeDefs = gql`
     bookCount: Int
     savedBooks: [Book]
   }
+
+  type Book {
+    _id: ID
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String
+  }
 `;
