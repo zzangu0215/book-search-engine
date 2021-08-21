a8028eb4d58d37992d2ba1b504c5eb57d50b4d51
Junny <tajo0215@gmail.com>
2021-08-21 13:39:48 -0700

    Query for resolvers implemented

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,16 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Thought } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate("savedBooks");
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+};
+
+module.exports = resolvers;
