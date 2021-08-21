381bff340ff01fe4edf37abec30c2bb0e0e5bda6
Junny <tajo0215@gmail.com>
2021-08-21 13:35:52 -0700

    typeDef finished

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -14,8 +14,35 @@ const typeDefs = gql`
     _id: ID
     authors: [String]
     description: String
+    title: String
     image: String
     link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(book: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+
+  input BookInput {
+    _id: ID
+    authors: [String]
+    description: String
     title: String
+    image: String
+    link: String
   }
 `;
+
+module.exports = typeDefs;
