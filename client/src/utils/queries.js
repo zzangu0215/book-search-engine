e73a1deaba9f36427765e569aaf7755d6c422603
Junny <tajo0215@gmail.com>
2021-08-21 13:53:59 -0700

    required npm packages installed to client side, queries.js implemented

---
+++ client/src/utils/queries.js
@@ -0,0 +1,17 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      username
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        title
+        description
+        image
+      }
+    }
+  }
+`;
