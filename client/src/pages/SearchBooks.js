02a52981fac52cd3c51acc42a85ba293bf27cc81
Junny <tajo0215@gmail.com>
2021-08-21 17:19:47 -0700

    import useMutation hook from Apllo client, and change saveBook function to what we imported

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -8,10 +8,12 @@ import {
   Card,
   CardColumns,
 } from "react-bootstrap";
+import { useMutation } from "@apollo/client";
 
-import Auth from "../utils/auth";
-import { saveBook, searchGoogleBooks } from "../utils/API";
+import { SAVE_BOOK } from "../utils/mutations";
+import { searchGoogleBooks } from "../utils/API";
 import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
+import Auth from "../utils/auth";
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -21,6 +23,8 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  const [saveBook] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -73,7 +77,8 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      // const response = await saveBook(bookToSave, token);
+      const response = await saveBook({ variables: { book: bookToSave } });
 
       if (!response.ok) {
         throw new Error("something went wrong!");
