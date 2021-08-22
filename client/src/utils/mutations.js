09732dea4efd8b2a8983c8f5e6ddf5a6115924ad
Junny <tajo0215@gmail.com>
2021-08-21 17:00:32 -0700

    LOGIN_USER mutation implemented

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,19 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER;
+
+export const SAVE_BOOK;
+
+export const REMOVE_BOOK;
