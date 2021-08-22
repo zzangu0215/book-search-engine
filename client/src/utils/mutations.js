585df3d6c30e6e85ee84921f310e26a0d864e509
Junny <tajo0215@gmail.com>
2021-08-21 17:02:18 -0700

    ADD_USER mutation implemented

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,7 +12,17 @@ export const LOGIN_USER = gql`
   }
 `;
 
-export const ADD_USER;
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email: password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
 
 export const SAVE_BOOK;
 
